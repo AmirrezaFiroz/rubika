13a2e9fa7dec7c5e4c19e957052ef2b9576207f0
Amirreza <amirreza.firoz13@gmail.com>
2023-03-13 12:17:24 +0330

    add function to get metaDAtas

--- Extension/Traits.php
+++ Extension/Traits.php
@@ -44,12 +44,12 @@ trait functions
      * @param string ...$chars
      * @return string
      */
-    public static function rand_str_chars(int $length, string ...$chars) : string
+    public static function rand_str_chars(int $length, string ...$chars): string
     {
         $str = '';
-        for ($i=0; $i < $length; $i++) { 
-            $t = mt_rand(0, count($chars) -1);
-            $str .= str_split($chars[$t])[mt_rand(0, strlen($chars[$t]) -1)];
+        for ($i = 0; $i < $length; $i++) {
+            $t = mt_rand(0, count($chars) - 1);
+            $str .= str_split($chars[$t])[mt_rand(0, strlen($chars[$t]) - 1)];
         }
         return $str;
     }
@@ -82,7 +82,7 @@ trait Texts
     public static function welcome(): void
     {
         System::clear();
-        echo Color::color(" rubika PHP client started ... \n     ||\n     || ", 'light_purple') . Color::color(Configs::get_github(), 'light_red') . Color::color("\n     ||\n     \_ ", 'light_purple') .Color::color(Configs::get_telegram(), 'light_red') . "\n";
+        echo Color::color(" rubika PHP client started ... \n     ||\n     || ", 'light_purple') . Color::color(Configs::get_github(), 'light_red') . Color::color("\n     ||\n     \_ ", 'light_purple') . Color::color(Configs::get_telegram(), 'light_red') . "\n";
     }
 
     /**
@@ -95,5 +95,38 @@ trait Texts
         System::clear();
         Printing::fast(Color::color(" starting >_ (98-" . substr_replace((string)$phone, '-****', 3, 4) . ")...", background: 'green') . "\n");
         sleep(1);
+    }
+
+    /**
+     * get markDown metadatas from text
+     *
+     * @param string $text text with metadatas
+     * @return array array of metadatas
+     */
+    function extract_markdown_metadata(string $text): array
+    {
+        $metadata = array();
+        $bold_pattern = '/\*\*(.+?)\*\*/';
+        $mono_pattern = '/\`(.+?)\`/';
+        $italic_pattern = '/\_\_(.+?)\_\_/';
+        if (preg_match_all($bold_pattern, $text, $matches, PREG_OFFSET_CAPTURE)) {
+            foreach ($matches[0] as $match) {
+                $metadata[] = array(
+                    'type' => 'bold',
+                    'index' => $match[1],
+                    'length' => strlen($match[0])
+                );
+            }
+        }
+        if (preg_match_all($italic_pattern, $text, $matches, PREG_OFFSET_CAPTURE)) {
+            foreach ($matches[0] as $match) {
+                $metadata[] = array(
+                    'type' => 'italic',
+                    'index' => $match[1],
+                    'length' => strlen($match[0])
+                );
+            }
+        }
+        return $metadata;
     }
 }
