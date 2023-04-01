eeea4c728bb224a2279eb99f618c14693003e1ac
Amirreza <amirreza.firoz13@gmail.com>
2023-04-01 20:16:30 +0430

    edit file

--- VoiceChats/VoiceChat.php
+++ VoiceChats/VoiceChat.php
@@ -1,7 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace VoiceChats;
+
+use Rubika\Types\Account;
 
 class VoiceChat
 {
+    function __construct(
+        Account $acc,
+        string $guid
+    ) {
+    }
 }
